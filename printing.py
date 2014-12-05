74d01f00bbb664af513b11a82b5250ce0bed47bf
Florian <florian.geigl@tugraz.at>
2014-12-05 15:17:51 +0100

    more colors

--- printing.py
+++ printing.py
@@ -31,11 +31,27 @@ class bcolors:
     ENDC = prefix + '[0m'
     gen_c = lambda x, ENDC=ENDC, prefix=prefix: ENDC + prefix + '[' + str(x) + 'm'
     HEADER = gen_c(95)
-    BLUE = gen_c(94)
-    GREEN = gen_c(92)
     WARNING = gen_c(93)
     FAIL = gen_c(91)
-    BLACK = gen_c('0;31')
+
+    BLACK = gen_c('0;30')
+    WHITE = gen_c('1;37')
+
+    BLUE = gen_c('0;34')
+    GREEN = gen_c('0;32')
+    PURPLE = gen_c('0;35')
+    RED = gen_c('0;31')
+    YELLOW = gen_c('1;33')
+    CYAN = gen_c('0;36')
+
+    DARK_GRAY = gen_c('1;30')
+
+    LIGHT_BLUE = gen_c('1;34')
+    LIGHT_GREEN = gen_c('1;32')
+    LIGHT_CYAN = gen_c('1;36')
+    LIGHT_RED = gen_c('1;31')
+    LIGHT_PURPLE = gen_c('1;35')
+    LIGHT_GRAY = gen_c('0;37')
 
 
 def color_string(string, type=bcolors.BLUE):
