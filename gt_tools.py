71f2d0590ef12a7dee55314bb6b23573088796cd
Florian <florian.geigl@tugraz.at>
2015-01-12 12:18:23 +0100

    mark new active nodes bugfix

--- gt_tools.py
+++ gt_tools.py
@@ -535,7 +535,7 @@ class GraphAnimator():
             old_pos_abs_update = pos_tmp_net.new_vertex_property('vector<float>')
             if self.mark_new_active_nodes:
                 colors = nodes_graph.new_vertex_property('vector<float>')
-                colors.a = np.array([np.array([1, 0, 0, 0.75]) if self.active_nodes[n] else np.array([0, 0, 1, 0.75]) for n in nodes_graph.vertices()])
+                colors.set_2d_array(np.array([np.array([1, 0, 0, 0.75]) if self.active_nodes[n] else np.array([0, 0, 1, 0.75]) for n in nodes_graph.vertices()]).T)
             for v in pos_tmp_net.vertices():
                 old_pos_update[v] = self.pos[v]
                 old_pos_abs_update[v] = self.pos_abs[v]
