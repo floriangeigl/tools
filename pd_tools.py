fe3e3b914baf3ff282a7cdb1dae77f3aad15b61e
Florian Geigl <florian.geigl@student.tugraz.at>
2015-11-28 11:41:07 +0100

    minor fix

--- pd_tools.py
+++ pd_tools.py
@@ -224,6 +224,7 @@ def to_sunburst_csv(df, filename='sunburst.csv', sep='/', end_name='end', start_
                     df_seq_col=None, df_count_col=None, bg_color='white', sort=('count', 'seq'),
                     ascending=(False, True), max_depth=None, iterations=0):
     start_point = start_point.replace('-', '')
+    end_name = end_name.replace('-', '')
     # creates a csv suitable for https://github.com/timelyportfolio/sunburstR
     if isinstance(df, pd.Series) or (isinstance(df, pd.DataFrame) and len(df.columns) == 1):
         if isinstance(df, pd.DataFrame):
