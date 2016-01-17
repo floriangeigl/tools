8d8ce03ec7c360985b50405159ba59b2788661b3
Florian Geigl <florian.geigl@tugraz.at>
2016-01-17 20:20:32 +0100

    try catch traceback wrapper added

--- basics.py
+++ basics.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import traceback
 
 
 def create_folder_structure(filename):
@@ -26,3 +27,15 @@ def find_files(base_dir, file_ending):
             root += '/'
         res.extend([root + i for i in filter(lambda x: x.endswith(file_ending), files)])
     return sorted(res)
+
+
+def try_catch_traceback_wrapper(func):
+    def inner(*args, **kwargs):
+        try:
+            ret_val = func(*args, **kwargs)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
+        return ret_val
+
+    return inner
